d3c522488212871de3d41b2412784c792a73e238
Fabrice Rochette <f@2060.io>
2025-05-20 13:30:30 -0500

    fixes

--- src/main/java/io/twentysixty/sa/res/c/CredentialTypeResource.java
+++ src/main/java/io/twentysixty/sa/res/c/CredentialTypeResource.java
@@ -1,12 +1,27 @@
 package io.twentysixty.sa.res.c;
 
+import java.util.List;
+
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
-import io.twentysixty.sa.client.res.c.CredentialTypeInterface;
+import io.twentysixty.sa.client.model.credential.CredentialType;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
 
 
 @RegisterRestClient
-public interface CredentialTypeResource extends CredentialTypeInterface {
+public interface CredentialTypeResource  {
 	
+	@POST
+	  @Path("/credential-types")
+	  @Produces("application/json")
+	  public void createCredentialType(CredentialType credentialType);
+
+	  @GET
+	  @Path("/credential-types")
+	  @Produces("application/json")
+	  public List<CredentialType> getAllCredentialTypes();
 	
 }
--- src/main/java/io/twentysixty/sa/res/c/MessageResource.java
+++ src/main/java/io/twentysixty/sa/res/c/MessageResource.java
@@ -1,18 +1,20 @@
 package io.twentysixty.sa.res.c;
 
-import org.eclipse.microprofile.rest.client.annotation.RegisterProvider;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
+import io.twentysixty.sa.client.model.message.BaseMessage;
+import io.twentysixty.sa.client.model.message.IdMessage;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
 
-import io.twentysixty.sa.client.res.c.MessageInterface;
-import io.verana.faucet.hologram.chatbot.svc.LoggingClientRequestFilter;
-import io.verana.faucet.hologram.chatbot.svc.LoggingClientResponseFilter;
 
-
-@RegisterProvider(LoggingClientRequestFilter.class)
-@RegisterProvider(LoggingClientResponseFilter.class)
 @RegisterRestClient
-public interface MessageResource extends MessageInterface {
+public interface MessageResource  {
 
+	@POST
+	  @Path("/v1/message")
+	  @Produces("application/json")
+	  public IdMessage sendMessage(BaseMessage message);
 	
 }
