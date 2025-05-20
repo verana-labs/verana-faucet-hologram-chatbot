28307a1cfd6369653a9e8a26f5d56599d3ac1d1a
Fabrice Rochette <f@2060.io>
2025-05-20 13:04:12 -0500

    updates

--- src/main/java/io/verana/faucet/hologram/chatbot/svc/LoggingClientRequestFilter.java
+++ src/main/java/io/verana/faucet/hologram/chatbot/svc/LoggingClientRequestFilter.java
@@ -13,15 +13,12 @@ public class LoggingClientRequestFilter implements ClientRequestFilter {
 	
 	private static Logger logger = Logger.getLogger(LoggingClientRequestFilter.class);
 
-	@ConfigProperty(name = "com.mobiera.bitdefender.apikey")
-	String apiKey;
 	
 	
 	@Override
     public void filter(ClientRequestContext requestContext) {
         //requestContext.getHeaders().add("my_header", "value");
         List<Object> auth = new ArrayList();
-        auth.add("ApiKey " + apiKey);
 		requestContext.getHeaders().put("Authorization", auth);
         logger.info("filter: " + requestContext.getUri());
         logger.info("filter: " + requestContext.getHeaderString("Authorization"));
